52ed14f3578668022d5b15f7b013c58e95a98ce3
daoxie <bimoadyatma245@gmail.com>
2026-01-18 05:19:33 +0700

    berhasil menghubungkan flutter ke firebash

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/login.dart';
 import 'controllers/chatController.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => ChatController(),
